e05506515c9d207294a3f4ad736547e91b0e9688
jonatan monti <jonatan.monti@hotmail.com>
2023-05-13 21:55:40 -0300

    create the classes and assign properties

---
+++ ProjectStore/ItemSale.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectStore
+{
+    public class ItemSale
+    {
+
+        #region Properties
+
+        private int amount;
+
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        private List<Product> products = new List<Product>();
+
+        public List<Product> Products
+        {
+            get { return products; }
+            set { products = value; }
+        }
+
+        #endregion
+
+    }
+}
---
+++ ProjectStore/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectStore
+{
+    public class Product
+    {
+
+        #region Properties
+
+        private string barcode;
+
+		public string Barcode
+		{
+			get { return barcode; }
+			set { barcode = value; }
+		}
+
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+		private string description;
+
+		public string Description
+		{
+			get { return description; }
+			set { description = value; }
+		}
+
+		private double price;
+
+		public double Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+
+        #endregion
+    }
+}
